4ec03756d43794ed82a12b9f414362285dac91f5
iGotABadIdea <rv17314@gmail.com>
2021-08-08 20:38:45 +0530

    like button created

---
+++ src/components/common/like.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Like = (props) => {
+    let classes="fas fa-heart";
+        if(!props.liked ) classes="far fa-heart";
+     return ( <i onClick={props.onClick} style={{cursor:'pointer'}}className={classes}></i> );
+}
+ 
+export default Like; 
