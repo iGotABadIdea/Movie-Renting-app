e9959908856b105225547249525dadbfb726402c
iGotABadIdea <rv17314@gmail.com>
2021-08-12 10:43:25 +0530

    Filtering completed

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from '../services/fakeGenreService';
-import Like from './common/like';
+import MoviesTable from './moviesTable'
+
 import Pagination from './common/pagination'
 import {paginate} from '../utils/paginate';
 import ListGroup from './common/listGroup'
@@ -13,7 +14,8 @@ class Movies extends Component {
         currentPage:1,
      };
      componentDidMount() {
-       this.setState({movies:getMovies(),genres:getGenres()});
+       const genres=[{name:"All Genres"},...getGenres()];
+       this.setState({movies:getMovies(),genres});
      }
      handleDelete=movie=>{
        const movies2=this.state.movies.filter(m=>(m._id!==movie._id))
@@ -30,13 +32,14 @@ class Movies extends Component {
        this.setState({currentPage:page})
      };
      handleGenreSelect=genre=>{
-       this.setState({selectedGenre:genre});
+       this.setState({selectedGenre:genre, currentPage:1});
      }
     render() {
       const {length:count}=this.state.movies;
       const {pageSize,currentPage,genres,selectedGenre,movies:allMovies}=this.state;
       if(count===0) return <p>There are no Movies in the data base</p>
-      const movies=paginate(allMovies,currentPage,pageSize)
+      const filtered=selectedGenre && selectedGenre._id ? allMovies.filter(m=>m.genre._id===selectedGenre._id):allMovies;
+      const movies=paginate(filtered,currentPage,pageSize)
         return (
               <div className='row'>
           <div className="col-3">
@@ -46,35 +49,11 @@ class Movies extends Component {
             onItemSelect={this.handleGenreSelect } />
           </div>
            <div className="col">
-             <p>Showing {count} movies in the data-base</p>
-           <table className="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Title</th>
-      <th scope="col">genre</th>
-      <th scope="col">Stock</th>
-      <th scope="col">Rate</th>
-      <th/>
-      <th/>
-    </tr>
-  </thead>
-  <tbody>
-      {movies.map(movie=>(
-    <tr key={movie._id}>
-      <td>{movie.title}</td>
-      <td>{movie.genre.name}</td>
-      <td>{movie.numberInStock}</td>
-      <td>{movie.dailyRentalRate}</td>
-      <td><Like liked={movie.liked} onClick={()=>this.handleLike(movie)}/></td>
-      <td><button onClick={()=>this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
-    </tr>
+             <p>Showing {filtered.length} movies in the data-base</p>
+             <MoviesTable movies={movies} onLike={this.handleLike} onDelete={this.handleDelete}/>
 
-      ))}
-
-
-  </tbody>
-</table> 
-<Pagination itemsCount={count} pageSize={pageSize} onPageChange={this.handlePageChange}
+           
+<Pagination itemsCount={filtered.length} pageSize={pageSize} onPageChange={this.handlePageChange}
 currentPage={currentPage}/>
           </div>
         </div>
---
+++ src/components/moviesTable.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Like from './common/like';
+const MoviesTable = (props) => {
+    const {movies,onDelete,onLike} = props;
+    return ( 
+        <table className="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Title</th>
+      <th scope="col">genre</th>
+      <th scope="col">Stock</th>
+      <th scope="col">Rate</th>
+      <th/>
+      <th/>
+    </tr>
+  </thead>
+  <tbody>
+      {movies.map(movie=>(
+    <tr key={movie._id}>
+      <td>{movie.title}</td>
+      <td>{movie.genre.name}</td>
+      <td>{movie.numberInStock}</td>
+      <td>{movie.dailyRentalRate}</td>
+      <td><Like liked={movie.liked} onClick={()=>onLike(movie)}/></td>
+      <td><button onClick={()=>onDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
+    </tr>
+
+      ))}
+
+
+  </tbody>
+</table> 
+     );
+}
+ 
+export default MoviesTable;
