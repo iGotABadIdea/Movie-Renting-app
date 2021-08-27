b6f90ca1b5eea09a131a38038b31dbc5ebaaebec
iGotABadIdea <rv17314@gmail.com>
2021-08-27 14:08:12 +0530

    Route added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import Movies from "./components/movies"
+import Movies from "./components/movies";
+import Customers from './components/customers'
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
 import {Route} from "react-router-dom";
 import React, { Component } from "react";
+import NavBar from './components/common/navbar';
 
 import './App.css';
 
@@ -8,11 +12,11 @@ class App extends Component {
   render () {
     return (
       <main className="container">
-              <Movies/>
-      <Route path=" component="></Route>
-      <Route path=" component="></Route>
-      <Route path=" component="></Route>
-      <Route path=" component="></Route>
+      <NavBar/>
+      <Route path="/movies" component={Movies}></Route>
+      <Route path="/customers" component={Customers}></Route>
+      <Route path="/rentals" component={Rentals}></Route>
+      <Route path="/notFound" component={NotFound}></Route>
       </main>
 
     );
