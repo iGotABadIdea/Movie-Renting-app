e60244b13174f711edb690ab095240ac0208c5ee
iGotABadIdea <rv17314@gmail.com>
2021-09-02 12:56:53 +0530

    form added

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 class LoginForm extends Component {
-    state = {  }
+    handleSubmit = e =>{
+        e.preventDefault();
+        console.log('Submitted');
+    
+    };
     render() { 
         return ( <div>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={this.handleSubmit}>
             <div className="form-group"><label htmlFor="username"></label>Username<input id="username" type="text" className="form-control"/></div>
             <div className="form-group"><label htmlFor="password"></label>Password<input id="password" type="text" className="form-control"/></div>
-            <button type="button" class="btn btn-primary">Primary</button>
+            <button type="button" className="btn btn-primary loginButton">Login</button>
             </form>
         </div> );
     }
