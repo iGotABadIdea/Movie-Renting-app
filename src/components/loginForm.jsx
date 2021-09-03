15227c897d4aa1680febd69aceb641cc646b29ef
iGotABadIdea <rv17314@gmail.com>
2021-09-03 10:59:22 +0530

    updated

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 class LoginForm extends Component {
+   state={
+    account:{username:'', password:''}
+   }
+   handleChange=({currentTarget:input})=>{
+       const account={...this.state.account};
+       account[input.name]=input.value;
+       this.setState({account});
+   };
     handleSubmit = e =>{
         e.preventDefault();
-        console.log('Submitted');
+        const username=this.username.current.value;
     
     };
     render() { 
+        const {account}=this.state;
         return ( <div>
             <h1>Login</h1>
             <form onSubmit={this.handleSubmit}>
-            <div className="form-group"><label htmlFor="username"></label>Username<input id="username" type="text" className="form-control"/></div>
-            <div className="form-group"><label htmlFor="password"></label>Password<input id="password" type="text" className="form-control"/></div>
+            <div className="form-group"><label htmlFor="username">Username</label>
+            <input value={account.username} onChange={this.handleChange} id="username" name="username" type="text" className="form-control"/></div>
+            <div className="form-group"><label htmlFor="password">Password</label>
+            <input value={account.password} onChange={this.handleChange} id="password" name="password" type="text" className="form-control"/></div>
             <button type="button" className="btn btn-primary loginButton">Login</button>
             </form>
         </div> );
