89a9b65a8fe2e1780bb9bf98428a5adf8152eb68
iGotABadIdea <rv17314@gmail.com>
2021-09-10 12:26:56 +0530

    form updated

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser'
 import Input from './common/input';
-class LoginForm extends Component {
+import Form from './common/form';
+class LoginForm extends Form {
    state={
     data:{username:'', password:''},
     errors:{}
@@ -10,41 +11,13 @@ class LoginForm extends Component {
        username:Joi.string().required().label('Username'),
        password:Joi.string().required().label('Password')
    }
-   validateProperty=({name,value})=>{
-      const obj={[name]:value};
-      const schema={[name]:this.schema[name]};
-      const {error}=Joi.validate(obj,schema);
-      return error ? error.details[0].message : null;
-   };  
-   handleChange=({currentTarget:input})=>{
-        const errors={...this.state.errors};
-        const errorMessage=this.validateProperty(input);
-        if(errorMessage) errors[input.name]=errorMessage;
-        else delete errors[input.name];
-
-
-       const data={...this.state.data};
-       data[input.name]=input.value;
-       this.setState({data,errors});
-   };
-   validate=()=>{
-       const option={abortEarly:false};
-       const {error}=Joi.validate(this.state.data,this.schema,option);
-       if(!error) return null;
-       const errors={};
-       for(let item of error.details)
-       errors[item.path[0]]=item.message;
-        return errors;
-      
-   }
-    handleSubmit = e =>{
-        e.preventDefault();
-        const errors=this.validate();
-        this.setState({errors:errors || {} });
-        if(errors) return;
-        console.log("submitted");
-
-    };
+   
+   
+  
+    
+    doSubmit=()=>{
+    console.log("submitted");
+    }
     render() { 
         const {data,errors}=this.state;
         return ( <div>
