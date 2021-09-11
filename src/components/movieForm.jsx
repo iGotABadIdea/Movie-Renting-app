5b5ae843c3fe310c1253d0af945c4f9b60be9936
iGotABadIdea <rv17314@gmail.com>
2021-09-11 16:59:10 +0530

    updated

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,19 +1,45 @@
 import React, { Component } from 'react'
 import Form from './common/form';
+import {getMovie,saveMovie} from '../services/fakeMovieService';
+import {getGenres} from '../services/GenreService';
 import Joi from 'joi-browser';
 class MovieForm extends Form {
     state={
         data:{
             title:'',
+            genreId:'',
             numberInStock:'', 
             rate:''
         },
+        genres:[],
         errors:{}
     }
     schema={
+        _id:Joi.string(),
         title:Joi.string().required().label('Title'),
+        genreId:Joi.string().required().label('Genre'),
        numberInStock:Joi.number().integer().min(0).max(100).required().min(5).label('Number in Stock'),
-       rate:Joi.number().integer().min(0).max(10).required().label('Rate')
+       rate:Joi.number().integer().min(0).max(10).required().label('Daily rental Rate')
+    }
+    componentDidMount(){
+        const genres=getGenres()
+        this.setState({genres});
+
+        const movieId=this.props.match.params.id;
+        if(movieId==='new')return ;
+        const movie=getMovie(movieId)
+        if(!movie)return this.props.history.replace("/notFound");
+
+        this.setState({data:this.mapToViewModel(movie)});
+    }
+    mapToViewModel(movie){
+       return {
+           _id:movie._id,
+           title:movie.title,
+           genreId:movie.genre._id,
+           numberInStock:movie.numberInStock,
+           
+       }
     }
     render() { 
         return <div>
